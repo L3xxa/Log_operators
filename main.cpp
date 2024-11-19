85548a24c182f51df6a41365ddc7f30c6ea930f9
Alyosha Holovaty <alekseiholovati@gmail.com>
2024-11-19 13:21:05 +0200

    complete

--- main.cpp
+++ main.cpp
@@ -3,19 +3,20 @@
 using namespace std;
 
 int main() {
-    int a;
-    cout << "Enter a number: ";
-    cin >> a;
+	int a, b;
+	cout << "Enter a number: ";
+	cin >> a;
+	cout << "Enter another number: ";
+	cin >> b;
 
-    if (a > 0) {
-        cout << "Positive" << endl;
-    }
-    else if (a < 0) {
-        cout << "Negative" << endl;
-    }
-    else {
-        cout << "Zero" << endl;
-    }
-	_getch();
-    return 0;
+	if (a < b) {
+		cout << a << " is less than " << b << endl;
+	}
+	else if (a == b) { 
+		cout << a << " is equal to " << b << endl;
+	}
+	else {
+		cout << b << " is less than " << a << endl;
+	}
+	
 }
